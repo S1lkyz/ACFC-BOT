1bb7f3f4ef25d00e77386a5b07e4039c818c934c
S1lkyz <Nicholasjrh11@gmail.com>
2026-06-29 10:11:18 -0700

    Update bot activity name from 'DoorDash Community' to 'AC FC COMMUNITY'

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "DoorDash Community",
+        name: "AC FC COMMUNITY",
         // Activity type number (0 = Playing).
         type: 0, 
       },
