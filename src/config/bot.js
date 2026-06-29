65046d5690e62b4a23e5659477891434c83d80b1
S1lkyz <Nicholasjrh11@gmail.com>
2026-06-29 10:14:23 -0700

    Change bot activity name to 'AC FC COMMUNITY'
    Updated the activity name to remove the join link.

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,8 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "AC FC COMMUNITY JOIN
-          https://discord.gg/R6QsZ568t",
+        name: "AC FC COMMUNITY",
         // Activity type number (0 = Playing).
         type: 0, 
       },
