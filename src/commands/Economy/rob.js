e00d466315510029ffef1d94fead2d75d8664f44
S1lkyz <Nicholasjrh11@gmail.com>
2026-07-09 13:14:00 -0700

    Increase base rob success chance to 90%

--- src/commands/Economy/rob.js
+++ src/commands/Economy/rob.js
@@ -6,7 +6,7 @@ import { MessageTemplates } from '../../utils/messageTemplates.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 const ROB_COOLDOWN = 4 * 60 * 60 * 1000;
-const BASE_ROB_SUCCESS_CHANCE = 0.0;
+const BASE_ROB_SUCCESS_CHANCE = 0.9;
 const ROB_PERCENTAGE = 0.15;
 const FINE_PERCENTAGE = 0.1;
 
