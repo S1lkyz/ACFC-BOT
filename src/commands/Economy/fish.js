c5b766ce091479ab3d68e6d78401faf460313166
S1lkyz <Nicholasjrh11@gmail.com>
2026-07-10 06:42:32 -0700

    Increase fishing reward range

--- src/commands/Economy/fish.js
+++ src/commands/Economy/fish.js
@@ -6,8 +6,8 @@ import { MessageTemplates } from '../../utils/messageTemplates.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 const FISH_COOLDOWN = 3 * 60 * 1000; 
-const BASE_MIN_REWARD = 300;
-const BASE_MAX_REWARD = 900;
+const BASE_MIN_REWARD = 10000;
+const BASE_MAX_REWARD = 100000;
 const FISHING_ROD_MULTIPLIER = 1.5;
 
 const FISH_TYPES = [
