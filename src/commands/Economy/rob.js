cadfcf068b752e7751455cd66fbad341d1a9cfac
S1lkyz <Nicholasjrh11@gmail.com>
2026-07-10 11:05:01 -0700

    Increase rob success chance and fine percentage

--- src/commands/Economy/rob.js
+++ src/commands/Economy/rob.js
@@ -6,9 +6,9 @@ import { MessageTemplates } from '../../utils/messageTemplates.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 const ROB_COOLDOWN = 1 * 60 * 60 * 1000;
-const BASE_ROB_SUCCESS_CHANCE = 0.0;
+const BASE_ROB_SUCCESS_CHANCE = 0.4;
 const ROB_PERCENTAGE = 0.15;
-const FINE_PERCENTAGE = 0.0;
+const FINE_PERCENTAGE = 0.8;
 
 export default {
     data: new SlashCommandBuilder()
